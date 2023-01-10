a1c537762136a6ecd061014e4ca23c2368cab0e0
samuel <samueltique17@gmail.com>
2023-01-10 11:54:21 -0500

    Agregando función darFormato al texto para verificar acentos y caracteres especiales

--- Js/script.js
+++ Js/script.js
@@ -2,8 +2,25 @@
 let imagen = document.getElementById("imagen1");
 let resultado = document.getElementById("areaR");
 let copiar = document.getElementById("copiar");
-let acentos = /[ÁÉÍÓÚÑáéíóúñ]/ 
 
+
+function darFormato(cadena){
+    let aMayus = cadena.replace(/[ÁÀÄÂ]/g,'A');
+    let aMinus = aMayus.replace(/[áàäâ]/g,'a');
+    let eMayus = aMinus.replace(/[ÉÈËÊ]/g,'E');
+    let eMinus = eMayus.replace(/[éèëê]/g,'e');
+    let iMayus = eMinus.replace(/[ÍÌÏÎ]/g,'I');
+    let iMinus = iMayus.replace(/[íìïî]/g,'i');
+    let oMayus = iMinus.replace(/[ÓÒÖÔ]/g,'O');
+    let oMinus = oMayus.replace(/[óòöô]/g,'o');
+    let uMayus = oMinus.replace(/[ÚÙÜÛ]/g,'U');
+    let uMinus = uMayus.replace(/[úùüû]/g,'u');
+    let enieMayus = uMinus.replace(/[Ñ]/g,'N');
+    let enieMinus = enieMayus.replace(/[ñ]/g,'n');
+    let resultado = enieMinus.replace(/['|°¬!^`~"#$%&/()Çç=?¿{}_,.´+<>¡¨*:;]/gi,'');
+
+    return resultado;
+}
 
 function encriptar(){
     let areaTexto = document.getElementById("text");
@@ -13,21 +30,23 @@ function encriptar(){
         document.getElementById("advertencia").style.fontSize= "16px"
         
     }
-    if(texto!=""){texto= texto.toLowerCase() 
-    texto=texto.replaceAll("e", "enter");
-    texto=texto.replaceAll( "o", "ober");
-    texto=texto.replaceAll( "i", "imes");
-    texto=texto.replaceAll("a","ai");
-    texto=texto.replaceAll( "u", "ufat");
-    console.log(texto)
-    let resultado = document.getElementById("result")
-    resultado.value = texto
-    document.getElementById("result").style.display= "inline"
-    document.getElementById("areaR").style.display = "none";
-    document.getElementById("imagen1").style.display = "none";
-    document.getElementById("copiar").style.display= "inline"
-    document.getElementById("advertencia").style.display = "none"
-    document.getElementById("text").value = ""
+    if(texto!=""){
+        texto= darFormato(texto) 
+        texto=texto.toLowerCase()
+        texto=texto.replaceAll("e", "enter");
+        texto=texto.replaceAll( "o", "ober");
+        texto=texto.replaceAll( "i", "imes");
+        texto=texto.replaceAll("a","ai");
+        texto=texto.replaceAll( "u", "ufat");
+        console.log(texto)
+        let resultado = document.getElementById("result")
+        resultado.value = texto
+        document.getElementById("result").style.display= "inline"
+        document.getElementById("areaR").style.display = "none";
+        document.getElementById("imagen1").style.display = "none";
+        document.getElementById("copiar").style.display= "inline"
+        document.getElementById("advertencia").style.display = "none"
+        document.getElementById("text").value = ""
     }
 }
 
@@ -39,22 +58,24 @@ function desencriptar(){
         document.getElementById("advertencia").style.color= "red"
         
     }
-    if(texto!=""){texto= texto.toLowerCase() 
-    texto=texto.replaceAll("enter","e" );
-    texto=texto.replaceAll( "ober","o" );
-    texto=texto.replaceAll( "imes","i" );
-    texto=texto.replaceAll("ai","a");
-    texto=texto.replaceAll( "ufat","u" );
-    console.log(texto)
-    let resultado = document.getElementById("result")
-    resultado.value = texto
-    document.getElementById("result").style.display= "inline"
-    document.getElementById("areaR").style.display = "none";
-    document.getElementById("imagen1").style.display = "none";
-    document.getElementById("copiar").style.display= "inline"
-    document.getElementById("advertencia").style.display = "none"
-    document.getElementById("advertencia").style.fontSize= "16px"
-    document.getElementById("text").value = ""
+    if(texto!=""){texto= 
+        texto= darFormato(texto)
+        texto=texto.toLowerCase() 
+        texto=texto.replaceAll("enter","e" );
+        texto=texto.replaceAll( "ober","o" );
+        texto=texto.replaceAll( "imes","i" );
+        texto=texto.replaceAll("ai","a");
+        texto=texto.replaceAll( "ufat","u" );
+        console.log(texto)
+        let resultado = document.getElementById("result")
+        resultado.value = texto
+        document.getElementById("result").style.display= "inline"
+        document.getElementById("areaR").style.display = "none";
+        document.getElementById("imagen1").style.display = "none";
+        document.getElementById("copiar").style.display= "inline"
+        document.getElementById("advertencia").style.display = "none"
+        document.getElementById("advertencia").style.fontSize= "16px"
+        document.getElementById("text").value = ""
     }
 }
 
